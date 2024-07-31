a943ba4469ff32f6c036b49677079c2aa69e2eed
ShimuGuyue <2147539031@qq.com>
2024-07-31 15:09:21 +0800

    Add files via upload

---
+++ 630A/630Acode.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int Again_Twenty_Five_(int n);
+
+int main(void)
+{
+	int n;
+	std::cin >> n;
+	std::cout << Again_Twenty_Five_(n) << std::endl;
+	return 0;
+}
+
+int Again_Twenty_Five_(int n)
+{
+	return 25;
+}
